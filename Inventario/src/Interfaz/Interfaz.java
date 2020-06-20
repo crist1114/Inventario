80fa392140d479207dddbcce1245540f2a34e74f
johan leon <johan leon@LAPTOP-64CAQBBR>
2020-06-20 18:56:18 -0500

    armando la interfaz

--- Inventario/src/Interfaz/Interfaz.java
+++ Inventario/src/Interfaz/Interfaz.java
@@ -5,6 +5,9 @@
  */
 package Interfaz;
 
+import inventario.Inventario;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Cristian
@@ -15,7 +18,8 @@ public class Interfaz extends javax.swing.JFrame {
      * Creates new form Interfaz
      */
     public Interfaz() {
-        initComponents();
+        initComponents(); 
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -28,39 +32,61 @@ public class Interfaz extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        btnAplicar2 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
+        btnVenderProducto = new javax.swing.JButton();
+        btnOrdenarPedido = new javax.swing.JButton();
+        btnMostrarEstadisticas = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        btnAplicar2.setText("Aplicar");
-        btnAplicar2.addActionListener(new java.awt.event.ActionListener() {
+        btnVenderProducto.setText("Vender Producto");
+        btnVenderProducto.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnAplicar2ActionPerformed(evt);
+                btnVenderProductoActionPerformed(evt);
             }
         });
+
+        btnOrdenarPedido.setText("Ordenar Pedido");
+        btnOrdenarPedido.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnOrdenarPedidoActionPerformed(evt);
+            }
+        });
+
+        btnMostrarEstadisticas.setText("Mostrar Estadisticas");
+        btnMostrarEstadisticas.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMostrarEstadisticasActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\johan leon\\Documents\\NetBeansProjects\\Inventario\\Inventario\\src\\Imagenes\\logo.jpg")); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(265, 265, 265)
-                .addComponent(btnAplicar2)
-                .addContainerGap(376, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(120, 120, 120))
+                .addContainerGap(107, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnMostrarEstadisticas, javax.swing.GroupLayout.PREFERRED_SIZE, 500, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnOrdenarPedido, javax.swing.GroupLayout.PREFERRED_SIZE, 500, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnVenderProducto, javax.swing.GroupLayout.PREFERRED_SIZE, 500, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addGap(99, 99, 99))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(140, Short.MAX_VALUE)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(135, 135, 135)
-                .addComponent(btnAplicar2)
-                .addGap(86, 86, 86))
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 289, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addComponent(btnVenderProducto)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnOrdenarPedido)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnMostrarEstadisticas)
+                .addGap(50, 50, 50))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -71,18 +97,38 @@ public class Interfaz extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnAplicar2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAplicar2ActionPerformed
-       
-    }//GEN-LAST:event_btnAplicar2ActionPerformed
+    private void btnVenderProductoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVenderProductoActionPerformed
+        Inventario i = new Inventario();
+        String nombreProducto = JOptionPane.showInputDialog("nombre del producto");
+        String cantidad = JOptionPane.showInputDialog("Cantidad");
+        int cantidadInt = Integer.parseInt(cantidad);
+        i.venderProducto(cantidadInt, nombreProducto);
+        
+    }//GEN-LAST:event_btnVenderProductoActionPerformed
+
+    private void btnOrdenarPedidoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOrdenarPedidoActionPerformed
+//        private String nombre;
+//    private String tipo;
+//    private int unidadesDisp;
+//    private int nNuevoPedido;
+//    private int precioBase;
+      
+      
+    }//GEN-LAST:event_btnOrdenarPedidoActionPerformed
+
+    private void btnMostrarEstadisticasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMostrarEstadisticasActionPerformed
+        InterfasPrueba interfas = new InterfasPrueba();
+        interfas.setBounds(0,0,540,400);
+        interfas.setVisible(true);
+        interfas.setResizable(false);
+        interfas.setLocationRelativeTo(null);
+    }//GEN-LAST:event_btnMostrarEstadisticasActionPerformed
 
     /**
      * @param args the command line arguments
@@ -120,8 +166,10 @@ public class Interfaz extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnAplicar2;
+    private javax.swing.JButton btnMostrarEstadisticas;
+    private javax.swing.JButton btnOrdenarPedido;
+    private javax.swing.JButton btnVenderProducto;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
