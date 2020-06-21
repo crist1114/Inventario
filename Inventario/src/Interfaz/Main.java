ab9146b1df140fd8e866ff97b0e098bc2adafb17
johan leon <johan leon@LAPTOP-64CAQBBR>
2020-06-21 18:18:39 -0500

    le pusimos main a la interfaz

---
+++ Inventario/src/Interfaz/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaz;
+
+/**
+ *
+ * @author johan leon
+ */
+public class Main {
+   public static void main(String args[]) {
+        Interfaz i = new Interfaz();
+        i.setVisible(true);
+    }
+}
